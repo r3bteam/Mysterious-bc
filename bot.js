2d8f5384c578e95b78df1e636e5d41c69d5cdce7
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-09-13 15:06:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,11 +110,11 @@ client.on('message', message => {
   });
 
 client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('487640610040446996');
+  const channel = client.channels.get('489768340839989258');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Mysterious Voice : ${currentSize}`);
-  if (currentSize !== size) channel.setName(`Mysterious Voice : ${currentSize}`);
+  if (!size) return channel.setName(`Voice Online : ${currentSize}`);
+  if (currentSize !== size) channel.setName(`Voice Online : ${currentSize}`);
 });
 
 
